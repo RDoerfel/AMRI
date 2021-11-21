e9c66f514cafd9d5135a29a6281cbb6c6ed36555
Teresa Cunha <94701072+TeresaMCunha@users.noreply.github.com>
2021-11-21 01:57:25 +0100

    Add files via upload
    reconstruction.m -> with circularly shifted matrices 
    showGrid.m -> just commented the 'figure' command to be able to set different window sizes in reconstruction.m

--- reconstruction.m
+++ reconstruction.m
@@ -5,10 +5,10 @@ addpath('./functions');
 
 %% Global Variables
 nXCal = 256;
-nYCal = 128;
+nYCal = 128; % Right now: 256 (it will be overwritten by 'maskCalibration.mat')
 nCoils = 4;
 
-%% Sensitivity Maps
+% Sensitivity Maps
 load('data/calibration_4_coils.mat');
 load('data/mask.mat');
 load('data/maskCalibration.mat');
@@ -22,11 +22,21 @@ Mxy = Mx + 1i*My;
 [sensitivityMaps] = computeCoilSensitivities(stackImageCal);
 imageReconstructed = lsReconstruct(stackImageCal,sensitivityMaps);
 
-figure;
+figure("Units", "Normalized","Position", [0.3, 0.2, 0.4, 0.7])
 imagesc(rescale(abs(imageReconstructed).*maskCalibration));
 colormap('gray');
 
+figure("Units", "Normalized","Position", [0.05, 0.1, 0.4, 0.7])
 showGrid(abs(sensitivityMaps).*maskCalibration,'gray');
+sgtitle("Original Sensitivity Maps")
+
+figure("Units", "Normalized","Position", [0.45, 0.1, 0.4, 0.7])
+sensitivityMaps = circshift(sensitivityMaps, -floor(nXCal/2), 1);
+sgtitle({' Sensitivity Maps circularly shifted along the y direction','(central row now at the top)'})
+
+showGrid(abs(sensitivityMaps).*circshift(maskCalibration, -floor(nXCal/2), 1),'gray');
+%%
+figure
 showGrid(abs(stackImageCal).*maskCalibration,'gray');
 
 %% PI
@@ -39,10 +49,20 @@ Mxy = Mx + 1i*My;
 
 [stackKspacePI,stackImagePI] = getMatrices(Mxy,nXPi,nYPi,nCoils);
 
+figure("Units", "Normalized","Position", [0.05, 0.1, 0.4, 0.7])
 showGrid(abs(stackImagePI),'gray');
+sgtitle("Aliased images from each coil")
 
-%% Sense
+figure("Units", "Normalized","Position", [0.45, 0.1, 0.4, 0.7])
+stackImagePI = circshift(stackImagePI, -floor(nYPi/2), 1);
+showGrid(abs(stackImagePI),'gray');
+sgtitle({'Aliased images from each coil circularly shifted along the y direction','(central row now at the top)'})
+
+%% SENSE Reconstruction
+
+% Initialize the full-FOV image:
 imageSense = zeros(nYCal,nXCal);
+
 for y = 1:nYPi
     % loop over the entire left-right extent
     for x = 1:nXCal
@@ -52,17 +72,21 @@ for y = 1:nYPi
         S = transpose(S);
         % compute unfolding matrix U
         U = pinv(S);
-        % get Image values
+        % get values of current pixel in each coil's aliased image:
         a = stackImagePI(y,x,:);
         a = squeeze(a);
-        % compute original image values
+        % compute original image values at pixels (x,y) and (x,y+FOV/2)
         v = U*a;
-        % place into image
-        imageSense([y y + nXCal/2],x) =  [v(1),v(2)];
+        % place into the full-FOV image
+        imageSense([y y + nXCal/2],x) =  [v(1), v(2)];
+        
     end
 end
-figure;
-imagesc(rescale(abs(imageSense)));
+
+% Circularly shift back the image (central line in the image = central row in the matrix)
+imageSense = circshift(imageSense, +nYCal/2, 1);
+figure
+imagesc(rescale(abs(imageSense))); colormap('gray'); axis image
 hold on;
 plot(100,1,'*','MarkerSize', 10,'LineWidth',1,'color','red');
 plot(100,1+nXCal/2,'*','MarkerSize', 10,'LineWidth',1,'color','red');
