dedaf17582628ceb913bcd5b4e429c540f0d7bb1
RDoerfel <doerfelruben@aol.com>
2021-11-30 18:59:25 +0100

    add data and new sense function

--- functions/reconstructSense.m
+++ functions/reconstructSense.m
@@ -1,30 +1,44 @@
 function [imageSense,gMap] = reconstructSense(sensitivityMaps,stackImagePI,Gamma,R)
     [nYCal,nXCal,nCoils] = size(sensitivityMaps);
+    [nYPi,nXPi,~] = size(stackImagePI);
+
+    R = round(nYCal/nYPi); % Acceleration Factor
+    shifted_CoilImages = circshift(stackImagePI, -floor(nYPi/2), 1);
+    shifted_sensMaps = circshift(sensitivityMaps, -floor(nYCal/2), 1);
+    
     imageSense = zeros(nYCal,nXCal);
     gMap = zeros(nYCal,nXCal);
     if nargin < 4
         Gamma = eye(nCoils,nCoils);
     end
-    for y = 1:floor(nXCal/R)
+    for y = 1:nYPi
         % loop over the entire left-right extent
-        y_idx = y:floor(nXCal/R):nXCal;
+        y_idx = y:nYPi:nYPi*R
         for x = 1:nXCal
+            overlappingRows = zeros(R,1);
+            overlappingRows(1) = y;
+            for i = 2 : R
+                overlappingRows(i) = overlappingRows(i-1) + floor(nYCal/R);
+            end
+            
             % get seinsitivity matrix
-            S = sensitivityMaps([y_idx],x,:);
+            S = shifted_sensMaps(overlappingRows,x,:);
             S = squeeze(S);
             S = transpose(S);
             % compute unfolding matrix U
             U = computeUnfolding(S,Gamma);
             % get Image values
-            a = stackImagePI(y,x,:);
+            a = shifted_CoilImages(y,x,:);
             a = squeeze(a);
             % compute original image values
             v = U*a;
             % place into image
-            imageSense([y_idx],x) =  v;
+            imageSense([overlappingRows],x) =  v;
             % compute g values
-            gMap([y_idx],x) =  computeG(S,Gamma); 
+            gMap([overlappingRows],x) =  computeG(S,Gamma); 
         end
     end
+    imageSense = circshift(imageSense, +nYCal/2, 1);
+    gMap = circshift(gMap, +nYCal/2, 1);
 end
 
--- reconstructDiffR.m
+++ reconstructDiffR.m
@@ -2,73 +2,101 @@ clc;
 close all;
 clear all;
 addpath('./functions');
+addpath('./data');
+
+%% red Sensemaps
+for i = 1:8
+     a = h5read ('sensmaps.h5', sprintf('/maps/magnitude/%02d',i-1));
+     p = h5read ('sensmaps.h5', sprintf('/maps/phase/%02d',i-1));
+     SMAP(:,:,i)=fliplr(transpose(a.*exp(sqrt(-1)*(p))));
+end
 
 %% Global Variables
-nXCal = 256;
-nYCal = 256;
+nXCal = 128;
+nYCal = 64;
 nCoils = 8;
 
-M = load('data/calibration_8_coils.mat').M;
+M = load('data/spinEcho_EPI_8ch_CAL_10T2_0CS_0T2star.mat').M;
 mask = load('data/maskCalibration.mat').maskCalibration;
 
 Mxy = getMxy(M);
 stackKspace = getStackedKspace(Mxy,nXCal,nYCal,nCoils);
 stackImageCal = getStackedImage(stackKspace);
+stackImageCal = imresize(stackImageCal,[nXCal,2*nYCal]);
+
+sosImage = rootSOSFromStacked(stackImageCal);
+thresh = 0.05*max(abs(sosImage(:)));
+mask = abs(sosImage) > thresh;
+
 sensitivityMaps = computeCoilSensitivities(stackImageCal);
 
 %% Different Rs 
-MR2 = load('data/r2_8_coils.mat').M;
-nXCal = 256;
-nYCal = 128;
+MR2 = load('data/spinEcho_EPI_8ch_R2_10T2_0CS_0T2star.mat').M;
+nXPi = 128;
+nYPi = 64;
+R = 2;
 
 MxyR2 = getMxy(MR2);
-kspaceR2 = getStackedKspace(MxyR2,nXCal,nYCal,nCoils);
+kspaceR2 = getStackedKspace(MxyR2,nXPi,nYPi,nCoils);
+% kspaceR2 = zeroFillKspace(kspaceR2,R);
+
 imgR2 = getStackedImage(kspaceR2);
 
-R = 2;
 Gamma = eye(nCoils,nCoils);
 [imgSenseR2,gMapR2] = reconstructSense(sensitivityMaps,imgR2,Gamma,R);
 mean(abs(gMapR2(mask)))
 
 % visualize result
 plotImage(rescale(abs(imgSenseR2.*mask)),'gray');
-plotImage(medfilt2(abs(gMapR2.*mask)),'gray');
+plotImage(medfilt2(abs(gMapR2)),'hot');
 colorbar;
 
 % R3
-MR3 = load('data/r3_8_coils.mat').M;
-nXCal = 256;
-nYCal = 128;
+MR3 = load('data/spinEcho_EPI_8ch_R3_10T2_0CS_0T2star.mat').M;
+I =  load('data/spinEcho_EPI_8ch_R3_10T2_0CS_0T2star.mat').I;
+nXPi = I(2);
+nYPi = 42;
+R = 3;
 
 MxyR3 = getMxy(MR3);
-kspaceR3 = getStackedKspace(MxyR3,nXCal,nYCal,nCoils);
+kSpaceR3 = getStackedKspace(MxyR3,nXPi,nYPi,nCoils);
+%kSpaceR3 = zeroFillKspace(kSpaceR3,R);
+
 imgR3 = getStackedImage(kSpaceR3);
 
-R = 3;
 Gamma = eye(nCoils,nCoils);
 [imgSenseR3,gMapR3] = reconstructSense(sensitivityMaps,imgR3,Gamma,R);
-mean(abs(gMapR3(mask)))
+
+imgSenseR3 = imgSenseR3(~all(imgSenseR3 == 0, 2),:);
+gMapR3 = gMapR3(~all(gMapR3 == 0, 2),:);
+new_mask = mask(~all(imgSenseR3 == 0, 2),:);
+
+
+mean(abs(gMapR3(new_mask)))
 
 % visualize result
-plotImage(rescale(abs(imgSenseR3.*mask)),'gray');
-plotImage(medfilt2(abs(gMapR3.*mask)),'gray');
+plotImage(rescale(abs(imgSenseR3.*new_mask)),'gray');
+plotImage(medfilt2(abs(gMapR3)),'hot');
 colorbar;
 
 % R4
-MR4 = load('data/r4_8_coils.mat').M;
-nXCal = 256;
-nYCal = 128;
+MR4 = load('data/spinEcho_EPI_8ch_R4_10T2_0CS_0T2star.mat').M;
+I =  load('data/spinEcho_EPI_8ch_R4_10T2_0CS_0T2star.mat').I;
+nXPi= I(2);
+nYPi = length(I)-1;
+R = 4;
 
 MxyR4 = getMxy(MR4);
-kspaceR4 = getStackedKspace(MxyR4,nXCal,nYCal,nCoils);
+kSpaceR4 = getStackedKspace(MxyR4,nXPi,nYPi,nCoils);
+%kSpaceR4 = zeroFillKspace(kSpaceR4,R);
+
 imgR4 = getStackedImage(kSpaceR4);
 
-R = 4;
 Gamma = eye(nCoils,nCoils);
 [imgSenseR4,gMapR4] = reconstructSense(sensitivityMaps,imgR4,Gamma,R);
 mean(abs(gMapR4(mask)))
 
 % visualize result
 plotImage(rescale(abs(imgSenseR4.*mask)),'gray');
-plotImage(medfilt2(abs(gMapR4.*mask)),'gray');
+plotImage(medfilt2(abs(gMapR4)),'hot');
 colorbar;
